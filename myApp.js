488a56e5d99690f78551ac431cd2f2fd25bd3c59
Mladenka Orlić <mladenka.orlic@gmail.com>
2024-12-02 00:50:50 +0000

    their approach

--- myApp.js
+++ myApp.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 let mongoose=require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+let Person = mongoose.model("Person", personSchema);
 
-let PersonModel = require('./src/models/person');
-let Person=new PersonModel({name:"Mladenka",age:26,favoriteFoods:["bananas","apples"]});
 console.log(Person);
 
 const createAndSavePerson = (done) => {
